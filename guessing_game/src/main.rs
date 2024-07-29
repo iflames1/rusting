1c45b2abed2c86ebb5923f6188522489b4314545
Flames <isaacomenuche@gmail.com>
2024-07-29 04:13:33 +0100

    Quitting After a Correct Guess

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -7,24 +7,26 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    println!("The secret number is: {}", secret_number);
-
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-
-    let guess: u32 = guess.trim().parse().expect("Please enter a number!");
-
-    println!("You guessed: {guess}");
+    println!("The secret number is: {secret_number}");
 
     loop {
-        println!("Please input your guess.");
+        println!("Please input your guess: ");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = guess.trim().parse().expect("Please enter a number!");
+
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => println!("You win!")
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
         }
     }
 }
